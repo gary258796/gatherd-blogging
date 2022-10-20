f617728d652c94756a0b23f0b00171d3bc85f9dd
GaryLiao <gary258796@gmail.com>
2022-10-20 18:04:41 +0800

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          // { to: '/blog', label: 'Blog', position: 'left' },
 
           // 右側選項
           // {
